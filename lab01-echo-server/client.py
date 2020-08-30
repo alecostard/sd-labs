0f1091be4d06998992dd1c1c3260bcb6994a8b87
Alexandre <alecostard@gmail.com>
2020-08-30 19:40:18 -0300

    Refactor to use with resources syntax

--- lab01-echo-server/client.py
+++ lab01-echo-server/client.py
@@ -5,20 +5,17 @@ HOST = 'localhost'
 PORT = 5000
 RECV_BUFFER = 1024
 
-sock = socket.socket()
+with socket.socket() as sock:
+    sock.connect((HOST, PORT))
 
-sock.connect((HOST, PORT))
-
-print("digite [sair] para encerrar o programa")
-while True:
-    msg = input()
-    if msg == "[sair]":
-        break
-    elif not msg:
-        pass
-    else:
-        sock.send(msg.encode())
-        msg = sock.recv(RECV_BUFFER)
-        print(str(msg, encoding='utf-8'))
-
-sock.close()
+    print("digite [sair] para encerrar o programa")
+    while True:
+        msg = input()
+        if msg == "[sair]":
+            break
+        elif not msg:
+            pass
+        else:
+            sock.send(msg.encode())
+            msg = sock.recv(RECV_BUFFER)
+            print(str(msg, encoding='utf-8'))
--- lab01-echo-server/server.py
+++ lab01-echo-server/server.py
@@ -5,20 +5,17 @@ HOST = ''
 PORT = 5000
 RECV_BUFFER = 1024
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+with socket.socket() as sock:
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind((HOST, PORT))
+    sock.listen(1)
 
-sock.bind((HOST, PORT))
-sock.listen(1)
-conn, address = sock.accept()
+    conn, address = sock.accept()
+    with conn:
+        while True:
+            msg = conn.recv(RECV_BUFFER)
 
-while True:
-    msg = conn.recv(RECV_BUFFER)
-
-    if not msg:
-        break
-    else:
-        conn.sendall(msg)
-
-conn.close()
-sock.close()
+            if not msg:
+                break
+            else:
+                conn.sendall(msg)
